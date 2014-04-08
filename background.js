d36bc6dc3089f626f032f7f4fcd2cdbeb50c3a4b
Justin Clift <justin@gluster.org>
2014-04-08 02:44:53 +0100

    Added a popup to show the URL ID

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.commands.onCommand.addListener(function(command){
   if (command == "test") {
-    chrome.tabs.query({currentWindow: true, active: true}, function(tab){chrome.tabs.update(tab[0].id, {"url": "http://www.redhat.com"});})
-    //alert('Keyboard shortcut from extension worked!');
+    //chrome.tabs.query({currentWindow: true, active: true}, function(tab){chrome.tabs.update(tab[0].id, {"url": "http://www.redhat.com"});})
+    //chrome.tabs.query({currentWindow: true, active: true}, function(tab){chrome.tabs.update(tab[0].id, {selected: False});});
+    chrome.tabs.query({currentWindow: true, active: true}, function(tab){alert('Tab Id: ' + tab[0].id);});
     //chrome.tabs.create({"url": "http://www.gluster.org"});
   }
 });
