e8d31a51e2b5e6a95f12d7557dff5b7469aadd26
Justin Clift <justin@gluster.org>
2014-04-08 01:39:30 +0100

    Changed the javascript action to modify the in-place page url instead

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 chrome.commands.onCommand.addListener(function(command){
-	if (command == "test") {
-		alert('Keyboard shortcut from extension worked!');
-	}
+  if (command == "test") {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tab){chrome.tabs.update(tab[0].id, {"url": "http://www.redhat.com"});})
+    //alert('Keyboard shortcut from extension worked!');
+    //chrome.tabs.create({"url": "http://www.gluster.org"});
+  }
 });
