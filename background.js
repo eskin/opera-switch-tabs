eaa4a7375ad904fa11b8d5494537ed9a78a974de
Justin Clift <justin@gluster.org>
2014-04-08 03:09:31 +0100

    Added function code to switch to previous tab
    Doesn't wrap though, unlike the switch to next tab one

--- background.js
+++ background.js
@@ -1,4 +1,31 @@
 chrome.commands.onCommand.addListener(function(command){
+  if (command == "previoustab") {
+    // Copied from StackOverflow posting here:
+    // http://stackoverflow.com/questions/16276276/chrome-extension-select-next-tab
+    // First, get currently active tab
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+      if (tabs.length) {
+        var activeTab = tabs[0],
+        tabId = activeTab.id,
+        currentIndex = activeTab.index;
+
+        // Next, get number of tabs in the window, in order to allow cyclic previous
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+          var numTabs = tabs.length;
+
+          // Finally, get the index of the tab to activate and activate it
+          chrome.tabs.query({currentWindow: true, index: (currentIndex-1) % numTabs}, function(tabs){
+            if (tabs.length) {
+              var tabToActivate = tabs[0],
+              tabToActivate_Id = tabToActivate.id;
+              chrome.tabs.update(tabToActivate_Id, {active: true});
+            }
+          });
+        });
+      }
+    });
+  }
+
   if (command == "nexttab") {
     // Copied from StackOverflow posting here:
     // http://stackoverflow.com/questions/16276276/chrome-extension-select-next-tab
